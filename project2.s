5dd3e5ea115832351fc7dc30fee65acd85e8b124
StephenBurke <42814417+StephenBurke@users.noreply.github.com>
2019-04-10 18:58:49 -0500

    some structure

--- project2.s
+++ project2.s
@@ -2,31 +2,71 @@
 .func main
 
 .data
-    errorMessage: .asciz "Bail out - wiringPiSetupGpio() has failed!"
-    setupErrorCode = -1
+    buzzer = 15
+    gpioSetupErrorMessage: .asciz "Bail out - wiringPiSetupGpio() has failed!"
+    gpioSetupErrorCode = -1
 
     softToneErrorMessage: .asciz "setup softTone failed"
+    softToneErrorCode = -1
+
+    frequency: .word 28, 29, 31, 33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951, 4186
 
 
 .text
 main:
     push {lr}
-    bl wiringPiSetupGpio
-    cmp r0, #setupErrorCode
-    bne init_complete
-    ldr r0, = errorMessage
-    bl puts
-    mov r0, #setupErrorCode
+    bl gpioInit
+
+gpioInitComplete:
+    bl playScale
+    bl playRandom
     b done
-    
-init_complete:
+
+gpioInit:
     push {lr}
-    b playScale
-    b playRandom
+    bl wiringPiSetupGpio
+    cmp r0, #gpioSetupErrorCode
+    bne gpioInitComplete
+
+    ldr r0, = gpioSetupErrorMessage
+    bl puts
+    mov r0, #gpioSetupErrorCode
+    b done
 
 playScale:
+    push {lr}
+    @this block checks the softTone setup
+    mov r0, #buzzer
+    bl softToneCreate
+    cmp r0, #softToneErrorCode
+    bne softToneSuccess
+    ldr r0, =softToneErrorMessage
+    bl puts
+    @making noise
+    ldr r2, =frequency
+    mov r3, #0
+
+    mov r0, #buzzer
+    mov r1, #220
+    bl softToneWrite
+    mov r0, #250
+    bl delay
+
+    pop {lr}
+    bx lr 
 
 playRandom:
+    push {lr}
+    @this block checks the softTone setup
+    mov r0, #buzzer
+    bl softToneCreate
+    cmp r0, #softToneErrorCode
+    bne softToneSuccess
+    ldr r0, =softToneErrorMessage
+    bl puts
+
+    pop {lr}
+    bx lr
 
 done: 
     pop {lr}
