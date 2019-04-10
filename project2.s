b4c8fc73d364e7086635a8fd38f5e10ba7cbc839
StephenBurke <42814417+StephenBurke@users.noreply.github.com>
2019-04-10 17:38:11 -0500

    something

--- project2.s
+++ project2.s
@@ -2,27 +2,40 @@
 .func main
 
 .data
-    gpioErrorMessage: .asciz "Bail out - wiringPiSetupGpio() has failed!"
+    errorMessage: .asciz "Bail out - wiringPiSetupGpio() has failed!"
+    setupErrorCode = -1
+
     softToneErrorMessage: .asciz "setup softTone failed"
-    gpioSetupErrorCode = -1
+
 
 .text
 main:
     push {lr}
     bl wiringPiSetupGpio
-    cmp r0, #gpioSetupErrorCode
-    bne gpio_init_complete
-    ldr r0, = gpioErrorMessage
+    cmp r0, #setupErrorCode
+    bne init_complete
+    ldr r0, = errorMessage
     bl puts
-    mov r0, #gpioSetupErrorCode
+    mov r0, #setupErrorCode
     b done
     
-gpio_init_complete:
+init_complete:
     push {lr}
-    bl 
+    b playScale
+    b playRandom
 
+playScale:
+
+playRandom:
 
 done: 
     pop {lr}
     bx lr
 
+
+
+@ mov r0, (RANDOM NUMBER)
+@ mov r1, #88
+@ udiv r2, r0, r1 @ R2 <- R0 / R1
+@ mul r3, r1, r2, @ R3 <- R1 * R2 
+@ sub r0, r0, r3  @ R0 <- R0 - R3 @r3 and r0 were swapped
